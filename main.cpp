f2f71c6d4c5b2638e4567589e64dfb4b456c8b51
djossir <autry2293@gmail.com>
2025-03-02 13:29:54 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include "SFML/Graphics.hpp"
+
+
+
+
+
+int main() {
+
+	sf::Vector2f resolution(800, 600);
+
+	sf::VideoMode vm({800,600}, 32);
+	vm.bitsPerPixel = 32;
+	
+	sf::RenderWindow window(vm, "boilerplate");
+
+	sf::Texture texture;
+	sf::Sprite background();
+	if (!texture.loadFromFile("graphics/background.png")) {
+		std::cout << "Error loading texture" << std::endl;
+		return 1;
+	}
+	else {
+		std::cout << "Texture loaded" << std::endl;
+	}
+	while (window.isOpen()) {
+		sf::Sprite background(texture);
+		sf::CircleShape circle(100);
+	
+		circle.setFillColor(sf::Color::Green);
+
+		
+		window.clear();
+		window.draw(background);
+		window.draw(circle);
+		window.display();
+	}
+
+	return 0;
+}
